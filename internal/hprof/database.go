86a4330e7dd30df41d9dc5d495eaf94db1ef001d
sunnyst4r <ivan_ivanov_13.02@mail.ru>
2025-06-06 20:40:43 +0300

    add database connection

---
+++ internal/hprof/database.go
@@ -0,0 +1,42 @@
+package hprof
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+
+func InitDB() error {
+	dsn := "host=127.0.0.1 user=user password=password dbname=postgres port=15432 sslmode=disable TimeZone=UTC"
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to connect database: %w", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return fmt.Errorf("failed to get generic database object: %w", err)
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	if err := sqlDB.Ping(); err != nil {
+		return fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	log.Println("Successfully connected to the database!")
+	return nil
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
